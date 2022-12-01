1dbb47a2f2158c934a77b655b39e5584cd0cf56f
tektechnologies <info@tektechnologies.com>
2022-11-30 19:00:02 -0600

    add percentage results to li

--- js/app.js
+++ js/app.js
@@ -27,22 +27,36 @@ function Pizza(name, src){
   Pizza.allPizzasArray.push(this);
 
 }
-console.log('do we have Pizza? ',Pizza.allPizzasArray);
+// console.log('do we have Pizza? ',Pizza.allPizzasArray);
 
 // function helper for randomization
-function getRandomNumber(){
-  return Math.floor(Math.random() * Pizza.allPizzasArray.length);
-}
+// function getRandomNumber(){
+//   return Math.floor(Math.random() * Pizza.allPizzasArray.length);
+// }
 
 
 //function to render our pizzas
 function renderPizzas(){
-  let pizza1 = getRandomNumber();
-  let pizza2 = getRandomNumber();
-
-  while(pizza1 === pizza2){
-    pizza2 = getRandomNumber();
+  // let pizza1 = getRandomNumber();
+  // let pizza2 = getRandomNumber();
+  let pizza1 = Math.floor(Math.random() * Pizza.allPizzasArray.length);
+  let pizza2 = Math.floor(Math.random() * Pizza.allPizzasArray.length);
+  console.log(pizza1, pizza2);
+  // while(pizza1 === pizza2){
+  //   pizza2 = getRandomNumber();
+  // }
+  while(pizza1 === image1 || pizza1 === image2){
+    pizza1 = Math.floor(Math.random() * Pizza.allPizzasArray.length);
   }
+  while(pizza2 === image2 || pizza2 === image1){
+    pizza2 = Math.floor(Math.random() * Pizza.allPizzasArray.length);
+  }
+  //set up a ref to the pizza index array
+  // image1 = pizza1;
+  // image2 = pizza2;
+
+  console.log('first pizza',pizza1);
+  console.log('second pizza',pizza2);
 
   image1.src = Pizza.allPizzasArray[pizza1].src;
   image2.src = Pizza.allPizzasArray[pizza2].src;
@@ -84,13 +98,28 @@ function handlePizzaClick(event){
   }
 }
 
-
+//show everything on the button click is actually a div
 function renderResults(){
   let ul = document.querySelector('ul');
+
   for(let i = 0; i < Pizza.allPizzasArray.length; i++){
     let li = document.createElement('li');
+
     li.textContent = `${Pizza.allPizzasArray[i].name} had ${Pizza.allPizzasArray[i].views} views and was clicked on ${Pizza.allPizzasArray[i].click} times`;
+
     ul.appendChild(li);
+
+    let percentageListItem = document.createElement('li');
+    let math;
+    if(Pizza.allPizzasArray[i].click === 0){
+      math = `Zero clicks and shown ${Pizza.allPizzasArray[i].views} times.`;
+    } else {
+      math = Math.round(((Pizza.allPizzasArray[i].click / Pizza.allPizzasArray[i]['views']).toFixed(2) * 100)) + '%';
+    }
+
+    percentageListItem.textContent = `${Pizza.allPizzasArray[i].name} percentage of times clicked on vs times shown is ` + math;
+    ul.appendChild(percentageListItem);
+
   }
   //show our chart here
   showResultChart();
@@ -114,49 +143,50 @@ pizzaContainer.addEventListener('click', handlePizzaClick);
 function showResultChart(){
   // console.log(Pizza.allPizzasArray);
   // console.log('chart!!!!!!!!');
-  let labels = [];
-  let voteCounts = [];
-  let showCounts = [];
-  let votePercentage = [];
+  // let labels = [];
+  // let voteCounts = [];
+  // let showCounts = [];
+  // let votePercentage = [];
 
-  for(let i = 0; i < Pizza.allPizzasArray.length; i++){
-    //four arrays to update
-    // console.log('TEST from array',Pizza.allPizzasArray[i].name);
-    labels[i] = Pizza.allPizzasArray[i].name;
-    voteCounts[i] = Pizza.allPizzasArray[i].click;
-    showCounts[i] = Pizza.allPizzasArray[i].views;
-    //                             views / clicks 100 / 30
-    votePercentage[i] = Math.floor(100 * voteCounts[i] /showCounts[i] );
-  }
+  // for(let i = 0; i < Pizza.allPizzasArray.length; i++){
+  //   //four arrays to update
+  //   // console.log('TEST from array',Pizza.allPizzasArray[i].name);
+  //   labels[i] = Pizza.allPizzasArray[i].name;
+  //   voteCounts[i] = Pizza.allPizzasArray[i].click;
+  //   showCounts[i] = Pizza.allPizzasArray[i].views;
+  //   //                             views / clicks 100 / 30
+  //   votePercentage[i] = Math.floor(100 * voteCounts[i] /showCounts[i] );
+  // }
 
 
 
-  console.log('labels',labels);
-  console.log('voteCounts',voteCounts);
-  console.log('showCounts', showCounts);
-  console.log('votePercentage',votePercentage);
+  // console.log('labels',labels);
+  // console.log('voteCounts',voteCounts);
+  // console.log('showCounts', showCounts);
+  // console.log('votePercentage',votePercentage);
 
 
   const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
-    type: 'doughnut',
+    type: 'bar',
     data: {
-      labels: labels,
+      // labels: labels,
       datasets: [{
         label: 'Vote Count',
-        data: voteCounts,
+        // data: voteCounts,
         backgroundColor: 'rgb(0,0,200)',
       },
-      {
-        label: 'Times shown',
-        data: showCounts,
-        backgroundColor: 'rgb(200,0,0)',
-      },
-      {
-        label: 'Vote %',
-        data: votePercentage
-      }]
+      // {
+      //   label: 'Times shown',
+      //   data: showCounts,
+      //   backgroundColor: 'rgb(200,0,0)',
+      // },
+      // {
+      //   label: 'Vote %',
+      //   data: votePercentage
+      // }
+      ]
     },
     options: {
       scales: {
